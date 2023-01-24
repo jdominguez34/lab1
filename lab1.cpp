828091806b1200fe425a26544bb3ed1fe2b0693d
Student <student@lab-240-026.cs.csubak.edu>
2023-01-24 09:21:55 -0800

    my first commit for lab-1 j

---
+++ lab1.cpp
@@ -0,0 +1,21 @@
+//Jorge Dominguez
+//Spring 2023 
+//lab 1
+//
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+
+
+   int a = rand() % 10 + 1;
+   cout << a << endl;
+   cout << "Hello World this is my first git push\n"; 
+
+
+   
+
+   return 0;
+}
